e5e2a6fb6f4f22a2cb5efd8df17f03d89df4ce0f
Đức Anh Tài <datai28599@gmail.com>
2021-04-17 14:25:22 +0700

    topic table text -> menu item

--- src/pages/TopicScreen/main/TopicTable.js
+++ src/pages/TopicScreen/main/TopicTable.js
@@ -1,16 +1,29 @@
 import React from "react";
-import { View, StyleSheet } from "react-native";
-import { Divider, List, Text } from "@ui-kitten/components";
+import { View, StyleSheet, ListItem } from "react-native";
+import {
+  Divider,
+  List,
+  Text,
+  Button,
+  IndexPath,
+  Menu,
+  MenuItem,
+  Icon,
+  ImageProps,
+  Card,
+} from "@ui-kitten/components";
 
 import TopicBottom from "./TopicBottom";
 
-const data = new Array(8).fill({
+import { ArrowUpIcon, ArrowDownIcon } from "../../../components/icons";
+
+const data = new Array(5).fill({
   topicCode: "123",
   topicName: "Name of topic",
   guideTeacher: "Nguyen Thi Ai",
 
   semester: "202",
-  major: "Computer Science",
+  majors: ["Computer Science", "Computer Engineering"],
   educationMethod: "Formal",
   maxStudentTake: 3,
   minStudentTake: 1,
@@ -20,6 +33,8 @@ const data = new Array(8).fill({
   mainTask: "To do something",
   thesisTask: "Todo something when thesis",
   node: "note",
+
+  students: ["Nguyen Duc Anh Tai", "Tai Nguyen Duc Anh", "Duc Anh Tai"],
 });
 
 const TopicTable = () => {
@@ -44,48 +59,114 @@ const TopicTable = () => {
   );
 };
 
-const TopicRowHeader = () => {
+const TopicRowHeader = (props) => {
+  const [sortedIndex, setSortedIndex] = React.useState(new IndexPath(2));
+  const [sorted, setSorted] = React.useState(true);
+
   return (
     <View style={styles.topicRow}>
-      <Text style={[styles.column, styles.columnHeader, styles.code]}>
-        Code
-      </Text>
-      <Text style={[styles.column, styles.columnHeader, styles.name]}>
-        Name
-      </Text>
-      <Text style={[styles.column, styles.columnHeader, styles.guideTeacher]}>
-        Guide Teacher
-      </Text>
+      <MenuItem
+        style={[styles.column, styles.columnHeader, styles.code]}
+        accessoryRight={ArrowDownIcon}
+        title="Code"
+      />
+      <MenuItem
+        style={[styles.column, styles.columnHeader, styles.semester]}
+        accessoryRight={ArrowDownIcon}
+        title="Semester"
+      />
+      <MenuItem
+        style={[styles.column, styles.columnHeader, styles.majors]}
+        accessoryRight={ArrowDownIcon}
+        title="Majors"
+      />
+      <MenuItem
+        style={[styles.column, styles.columnHeader, styles.educationMethod]}
+        accessoryRight={ArrowDownIcon}
+        title=" Edu Method"
+      />
+      <MenuItem
+        style={[styles.column, styles.columnHeader, styles.topicName]}
+        accessoryRight={ArrowDownIcon}
+        title="Topic Name"
+      />
+      <MenuItem
+        style={[styles.column, styles.columnHeader, styles.guideTeacher]}
+        accessoryRight={ArrowDownIcon}
+        title="Guide Teacher"
+      />
+      <MenuItem
+        style={[styles.column, styles.columnHeader, styles.students]}
+        accessoryRight={ArrowDownIcon}
+        title="Students"
+      />
     </View>
   );
 };
 
-const TopicRow = ({ index, topic }) => {
-  return (
-    <View style={styles.topicRow}>
-      <Text style={[styles.column, styles.code]}>{topic.topicCode}</Text>
-      <Text style={[styles.column, styles.name]}>{topic.topicName}</Text>
-      <Text style={[styles.column, styles.guideTeacher]}>
-        {topic.guideTeacher}
-      </Text>
-    </View>
-  );
-};
+const TopicRow = ({ index, topic }) => (
+  <View style={styles.topicRow}>
+    <MenuItem style={[styles.column, styles.code]} title={topic.topicCode} />
+    <MenuItem style={[styles.column, styles.semester]} title={topic.semester} />
+    <List
+      style={[styles.column, styles.majors]}
+      data={topic.majors}
+      ItemSeparatorComponent={Divider}
+      renderItem={({ index, item }) => <MenuItem title={item} />}
+    />
+    <MenuItem
+      style={[styles.column, styles.educationMethod]}
+      title={topic.educationMethod}
+    />
+    <MenuItem style={[styles.column, styles.topicName]} title="Topic Name" />
+    <MenuItem
+      style={[styles.column, styles.guideTeacher]}
+      title="Guide Teacher"
+    />
+    <List
+      style={[styles.column, styles.students]}
+      data={topic.students}
+      ItemSeparatorComponent={Divider}
+      renderItem={({ index, item }) => <MenuItem title={item} />}
+    />
+  </View>
+);
 
 const styles = StyleSheet.create({
   topicRow: {
     flexDirection: "row",
+    justifyContent: "space-between",
   },
   column: {
-    margin: 5,
-    padding: 5,
+    margin: 3,
   },
   columnHeader: {
+    fontSize: 30,
+  },
+  code: {
+    width: 80,
     textAlign: "center",
   },
-  code: {},
-  name: { width: "25%" },
-  guideTeacher: { width: "20%" },
+  semester: {
+    width: 80,
+    textAlign: "center",
+  },
+  majors: {
+    width: 180,
+  },
+  educationMethod: {
+    width: 150,
+    textAlign: "center",
+  },
+  topicName: {
+    flex: 1,
+  },
+  guideTeacher: {
+    width: "15%",
+  },
+  students: {
+    width: "15%",
+  },
 });
 
 export default TopicTable;
