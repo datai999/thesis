d7e5fa10c279e528a3e43db08ed892105786e0c1
Đức Anh Tài <datai28599@gmail.com>
2021-04-26 00:07:58 +0700

    remove redundant package

--- src/components/form/topicCreateForm/TopicCreateLayout.js
+++ src/components/form/topicCreateForm/TopicCreateLayout.js
@@ -1,14 +1,6 @@
 import React from "react";
 import { StyleSheet } from "react-native";
-import * as Animatable from "react-native-animatable";
-import {
-  Modal,
-  Card,
-  Input,
-  Button,
-  Text,
-  Layout,
-} from "@ui-kitten/components";
+import { Button, Layout } from "@ui-kitten/components";
 
 import Props from "data/Props";
 
