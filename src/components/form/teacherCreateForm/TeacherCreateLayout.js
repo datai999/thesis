8e500d3533f8f89379b08a59fbcdffd7e43cf4df
Đức Anh Tài <datai28599@gmail.com>
2021-04-26 00:15:12 +0700

    teacher create form

--- src/components/form/teacherCreateForm/TeacherCreateLayout.js
+++ src/components/form/teacherCreateForm/TeacherCreateLayout.js
@@ -1,42 +1,62 @@
 import React from "react";
 import { StyleSheet } from "react-native";
 import * as Animatable from "react-native-animatable";
-import {
-  Modal,
-  Card,
-  Input,
-  Button,
-  Text,
-  Layout,
-} from "@ui-kitten/components";
+import { Text, Layout } from "@ui-kitten/components";
 
 import Props from "data/Props";
-import { MySelect, MyMultiSelect } from "components/Select";
-import BottomCard from "components/BottomCard";
-import { PlusIcon } from "components/Icons";
+import MyInput from "components/Input";
+import { MySelect } from "components/Select";
 
-const TeacherCreateLayout = (form) => (
-  <Layout>
-    <Text>Teacher</Text>
-  </Layout>
-);
+const TeacherCreateLayout = (form) => {
+  const setValue = (field, value) => (form[field] = value);
+  const inputProps = (field) => {
+    return {
+      value: form[field],
+      callBack: (value) => setValue(field, value),
+      ...Props[field],
+    };
+  };
+  const selectProps = (field) => {
+    return {
+      field,
+      value: form[field],
+      callBack: (value) => setValue(field, value),
+      ...Props[field],
+    };
+  };
+  return (
+    <Layout style={styles.container}>
+      <Layout style={styles.row}>
+        <Layout style={styles.left}>
+          <MyInput {...inputProps("name")} />
+          <MyInput {...inputProps("email")} />
+          <MyInput {...inputProps("phone")} />
+        </Layout>
+        <Layout style={styles.right}>
+          <MySelect {...selectProps("degree")} />
+          <MySelect {...selectProps("subjectDepartment")} />
+        </Layout>
+      </Layout>
+    </Layout>
+  );
+};
 
 const styles = StyleSheet.create({
-  modal: {
-    width: "50%",
-  },
-  backdrop: {
-    backgroundColor: "rgba(0, 0, 0, 0.5)",
-  },
-  card: {
+  container: {
     flex: 1,
-    justifyContent: "space-between",
   },
-  headerText: {
-    margin: 5,
-    fontWeight: "bold",
-    textAlign: "center",
-    fontSize: 30,
+  row: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-around",
+  },
+  left: {
+    flex: 1,
+    marginRight: 10,
+  },
+  right: {
+    flex: 1,
+    marginLeft: 10,
   },
 });
 
