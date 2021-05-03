7c2c964849540517f411665aed9aa5ce4c1acbae
Đức Anh Tài <datai28599@gmail.com>
2021-05-03 16:34:28 +0700

    remove log

--- src/api/ConstApi.js
+++ src/api/ConstApi.js
@@ -3,7 +3,6 @@ import AxiosClient from "./AxiosClient";
 const ConstApi = {
   getAll: () => {
     const url = "/const";
-    console.log("const" + AxiosClient.get(url));
     return AxiosClient.get(url);
   },
 };
