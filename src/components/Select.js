ee8fe40e3c691840d321a3b993d3702105a9562a
Đức Anh Tài <datai28599@gmail.com>
2021-05-03 19:12:03 +0700

    bug arrValue undefine

--- src/components/Select.js
+++ src/components/Select.js
@@ -1,21 +1,21 @@
+import { IndexPath, Select, SelectItem } from "@ui-kitten/components";
 import React from "react";
-import { StyleSheet } from "react-native";
-import * as Animatable from "react-native-animatable";
-import { Layout, Select, SelectItem, IndexPath } from "@ui-kitten/components";
 
 const selectItems = (arrItem) => {
-  return Array.from(arrItem, (item) => <SelectItem key={item} title={item} />);
+  return arrItem
+    ? Array.from(arrItem, (item) => <SelectItem key={item} title={item} />)
+    : null;
 };
 
 const MySelect = ({ field, callBack, ...props }) => {
   const [indexed, setIndexed] = React.useState(
-    new IndexPath(props.arrValue.indexOf(props.value))
+    new IndexPath(props.arrValue?.indexOf(props.value))
   );
 
   return (
     <Select
       {...props}
-      value={props.arrValue[indexed.row]}
+      value={props.arrValue && props.arrValue[indexed.row]}
       selectedIndex={indexed.row > -1 ? indexed : null}
       onSelect={(index) => {
         callBack(props.arrValue[index - 1]);
