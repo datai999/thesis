a886936a5ee610bb1c34f9bb5e46ea254da28b63
Đức Anh Tài <datai28599@gmail.com>
2021-05-20 22:47:15 +0700

    render multi lang

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,5 @@
+import i18n from "utils/i18n";
+
 function getRenderText(obj) {
   if (obj == null) return null;
   if (Array.isArray(obj)) return obj.map((x) => getRenderText(x));
@@ -6,7 +8,7 @@ function getRenderText(obj) {
     case "number":
       return obj;
     case "object":
-      return obj?.name ?? obj?.value;
+      return obj[i18n.language] ?? obj?.name ?? obj?.value;
     default:
       return null;
   }
