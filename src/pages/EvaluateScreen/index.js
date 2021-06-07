7967d398d2ed9cb39eb52150dac1fa6f066a4c2c
Đức Anh Tài <datai28599@gmail.com>
2021-06-07 13:32:40 +0700

    init evaluateScreen

---
+++ src/pages/EvaluateScreen/index.js
@@ -0,0 +1,13 @@
+import { Layout } from "@ui-kitten/components";
+import React from "react";
+import { Text } from "react-native";
+
+const EvaluateScreen = () => {
+  return (
+    <Layout>
+      <Text>Evaluate screen</Text>
+    </Layout>
+  );
+};
+
+export default EvaluateScreen;
