37e78051edbfec9d729320a7a217c7da02859d99
Đức Anh Tài <datai28599@gmail.com>
2021-06-09 00:05:23 +0700

    refactor common api

--- src/api/CommonApi.js
+++ src/api/CommonApi.js
@@ -1,21 +1,17 @@
 import AxiosClient from "./AxiosClient";
 
 const CommonApi = (baseURL = "") => {
-  let commonApi = new Object();
-
-  commonApi["getAll"] = () => {
-    return AxiosClient.get(baseURL);
+  return {
+    getAll: () => {
+      return AxiosClient.get(baseURL);
+    },
+    getPaging: (pageParam) => {
+      return AxiosClient.get(baseURL + "/paging", { params: pageParam });
+    },
+    create: (data) => {
+      return AxiosClient.post(baseURL, data);
+    },
   };
-
-  commonApi["getPaging"] = (pageParam) => {
-    return AxiosClient.get(baseURL + "/paging", { params: pageParam });
-  };
-
-  commonApi["create"] = (data) => {
-    return AxiosClient.post(baseURL, data);
-  };
-
-  return commonApi;
 };
 
 export default CommonApi;
