28b90ed82baeef4e12e476b7e40f79cef05d3e52
Đức Anh Tài <datai28599@gmail.com>
2021-06-09 21:43:18 +0700

    init TopNavigation

--- App.js
+++ App.js
@@ -1,10 +1,23 @@
-import React, { useState } from "react";
-import { ApplicationProvider, IconRegistry } from "@ui-kitten/components";
 import * as eva from "@eva-design/eva";
-import { SafeAreaProvider } from "react-native-safe-area-context";
+import {
+  ApplicationProvider,
+  Icon,
+  IconRegistry,
+  TopNavigation,
+  TopNavigationAction
+} from "@ui-kitten/components";
 import { EvaIconsPack } from "@ui-kitten/eva-icons";
-
+import React from "react";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 import Routes from "./src/Routes";
+
+const BackIcon = (props) => <Icon {...props} name="arrow-back" />;
+
+const SettingsIcon = (props) => <Icon {...props} name="settings" />;
+
+const renderSettingsAction = () => <TopNavigationAction icon={SettingsIcon} />;
+
+const renderBackAction = () => <TopNavigationAction icon={BackIcon} />;
 
 const App = () => {
   return (
@@ -12,6 +25,11 @@ const App = () => {
       <IconRegistry icons={EvaIconsPack} />
       <ApplicationProvider {...eva} theme={eva.light}>
         <SafeAreaProvider>
+          <TopNavigation
+            title="Eva Application"
+            accessoryLeft={renderBackAction}
+            accessoryRight={renderSettingsAction}
+          />
           <Routes />
         </SafeAreaProvider>
       </ApplicationProvider>
