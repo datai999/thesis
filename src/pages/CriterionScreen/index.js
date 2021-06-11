b38f4c7a9b966264427eb1a76b6cd66e850c05bb
Đức Anh Tài <datai28599@gmail.com>
2021-06-12 01:41:59 +0700

    template topic & thesis render

--- src/pages/CriterionScreen/index.js
+++ src/pages/CriterionScreen/index.js
@@ -1,10 +1,12 @@
-import { Divider, Layout, Text } from "@ui-kitten/components";
+import { Layout, Text } from "@ui-kitten/components";
+import { SettingApi } from "api/br";
 import CriterionTemplateApi from "api/score/CriterionTemplateApi";
 import { CreateBtn } from "components/Button";
 import { CriterionForm, CriterionTemplateForm } from "components/form";
 import OverTable from "components/screen/OverTable";
 import { TableContent } from "components/screen/TableData";
 import React from "react";
+import { StyleSheet } from "react-native";
 import i18n from "utils/i18n";
 
 const arrLink = [
@@ -18,15 +20,31 @@ const overTopBar = () => {
   return <CreateBtn form={CriterionForm} />;
 };
 
-function renderTopContent({ links, rowCallBack }) {
+function renderTopContent({ ...props }) {
+  const [topicTemplate, setTopicTemplate] = React.useState();
+  const [thesisTemplate, setThesisTemplate] = React.useState();
+
+  React.useEffect(() => {
+    const fetchData = () => {
+      SettingApi.getTemplate(false).then(setTopicTemplate);
+      SettingApi.getTemplate(true).then(setThesisTemplate);
+    };
+    fetchData();
+  }, []);
+
   return (
     <Layout>
-      <Text category="s2">{i18n.t("criterionTemplate.topicTemplate")}</Text>
-      <Divider />
-      <Text category="s2">{i18n.t("criterionTemplate.thesisTemplate")}</Text>
-      <Divider />
-      <TableContent links={links} data={[]} rowCallBack={rowCallBack} />
-      <Text category="s2">{i18n.t("criterionTemplate.allTemplate")}</Text>
+      <Text style={styles.group} category="s2">
+        {i18n.t("criterionTemplate.topicTemplate")}
+      </Text>
+      <TableContent {...props} data={topicTemplate?.setting} />
+      <Text style={styles.group} category="s2">
+        {i18n.t("criterionTemplate.thesisTemplate")}
+      </Text>
+      <TableContent {...props} data={thesisTemplate?.setting} />
+      <Text style={styles.group} category="s2">
+        {i18n.t("criterionTemplate.allTemplate")}
+      </Text>
     </Layout>
   );
 }
@@ -43,5 +61,11 @@ const overTableProps = {
 const CriterionScreen = () => {
   return <OverTable {...overTableProps} />;
 };
+
+const styles = StyleSheet.create({
+  group: {
+    marginTop: 30,
+  },
+});
 
 export default CriterionScreen;
