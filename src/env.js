d555e9ac2c4cb592635759dfbc59426e2758c703
Đức Anh Tài <datai28599@gmail.com>
2021-06-15 23:05:05 +0700

    env localStaging

--- src/env.js
+++ src/env.js
@@ -1,14 +1,26 @@
 import { Platform } from "react-native";
 
-let isLogin = Platform.OS == "android" ? true : true;
+let isLogin = Platform.OS == "android" ? true : false;
 
 const local = {
   domain: "http://localhost:19006/",
   baseURL: "http://192.168.15.112:8080/api",
-  androidLinkDown:   "https://drive.google.com/file/d/1Z6N8NdVO6vm0BNkRfPGr-GveHMTS9TLu",
+  androidLinkDown:
+    "https://drive.google.com/file/d/1Z6N8NdVO6vm0BNkRfPGr-GveHMTS9TLu",
   version: "6.15.20",
   isLogin: isLogin,
-  initialRouteName: isLogin ? "topic" : "home",
+  initialRouteName: isLogin ? "person" : "home",
 };
 
-export default local;
+const staging = {
+  ...local,
+  domain: "https://datai999.github.io/thesis/",
+  baseURL: "https://datai-thesis.herokuapp.com/api",
+};
+
+const localStaging = {
+  ...local,
+  baseURL: "https://datai-thesis.herokuapp.com/api",
+};
+
+export default localStaging;
