83ebee6c74fe2475710c6a547836a370efecc236
Đức Anh Tài <datai28599@gmail.com>
2021-06-15 20:01:37 +0700

    version 6.15.20

--- src/env.js
+++ src/env.js
@@ -6,7 +6,7 @@ const local = {
   domain: "http://localhost:19006/",
   baseURL: "http://192.168.15.112:8080/api",
   androidLinkDown:   "https://drive.google.com/file/d/1Z6N8NdVO6vm0BNkRfPGr-GveHMTS9TLu",
-  version: "6.15.18",
+  version: "6.15.20",
   isLogin: isLogin,
   initialRouteName: isLogin ? "topic" : "home",
 };
