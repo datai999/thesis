772b68931db9665db015cb36e85dd14fadac856a
Đức Anh Tài <datai28599@gmail.com>
2021-06-15 18:46:56 +0700

    version 6.15.18

--- src/env.js
+++ src/env.js
@@ -5,7 +5,7 @@ let isLogin = Platform.OS == "android" ? true : false;
 const local = {
   domain: "http://localhost:19006/",
   baseURL: "http://192.168.15.112:8080/api",
-  version: "6.15.16",
+  version: "6.15.18",
   isLogin: isLogin,
   initialRouteName: isLogin ? "topic" : "home",
 };
