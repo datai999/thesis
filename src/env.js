56f9575a5bfd6fcc9e78035dfd67e2cc7966638f
Đức Anh Tài <datai28599@gmail.com>
2021-06-17 00:56:59 +0700

    refactor env for android

--- src/env.js
+++ src/env.js
@@ -1,22 +1,13 @@
-import { Platform } from "react-native";
-
-let isLogin = Platform.OS == "android" ? true : false;
-
 const local = {
   baseURL: "http://192.168.15.112:8080/api",
   androidLinkDown:
     "https://drive.google.com/file/d/1Z6N8NdVO6vm0BNkRfPGr-GveHMTS9TLu",
   version: "6.17.00",
-  isLogin: isLogin,
-  initialRouteName: isLogin ? "topic" : "login",
+  isLogin: false,
+  initialRouteName: "login",
 };
 
 const staging = {
-  ...local,
-  baseURL: "https://datai-thesis.herokuapp.com/api",
-};
-
-const localStaging = {
   ...local,
   baseURL: "https://datai-thesis.herokuapp.com/api",
 };
