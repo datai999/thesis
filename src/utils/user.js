7e31f7c8776164644e2796a9670561fa7e43adc7
Đức Anh Tài <datai28599@gmail.com>
2021-06-17 00:03:33 +0700

    remove unese function user

--- src/utils/user.js
+++ src/utils/user.js
@@ -13,27 +13,11 @@ let userStorage = {
   code: false,
 };
 
-const actionCodeSettings = {
-  url: env.domain,
-  handleCodeInApp: true,
-};
-
-function sendLoginEmail(email) {
-  return firebase
-    .auth()
-    .sendSignInLinkToEmail(email, actionCodeSettings)
-    .then(() => {
-      AsyncStorage.setItem("emailForSignIn", email);
-    });
-}
-
 function navToHome(email) {
   if (email.substring(email.indexOf("@")) != emailTail) {
     toastHolder.error(email + i18n.t("toast.email.notHcmut"), { email });
     return;
   }
-
-  console.log("her");
 
   TeacherApi.postExample({ email: email }).then((response) => {
     userStorage.code = response[0]?.code;
@@ -42,46 +26,9 @@ function navToHome(email) {
   });
 }
 
-function loginWithEmailLink(email) {
-  firebase
-    .auth()
-    .signInWithEmailLink(email, window.location.href)
-    .then((result) => {
-      console.log("sign in result");
-      console.log(result);
-      AsyncStorage.removeItem("emailForSignIn");
-      navToHome(email);
-    })
-    .catch((error) => {
-      // TODO throw login error
-      console.log("signInWithEmailLink error");
-      console.log(error);
-      alert("signInWithEmailLink error" + error);
-    });
-}
-
-function createUserWithEmailPassword(email, password) {
-  return firebase
-    .auth()
-    .createUserWithEmailAndPassword(email, password)
-    .then((userCredential) => {
-      userStorage.user = userCredential.user;
-      console.log("createUserWithEmailPassword");
-      console.log(userStorage.user);
-    })
-    .catch((error) => toastHolder.errorCode(error.code, error));
-}
-
-function loginWithEmailPassword(email, password) {
-  return firebase
-    .auth()
-    .signInWithEmailAndPassword(email, password)
-    .then((userCredential) => {
-      userStorage.credential = userCredential.user;
-      userStorage.isLogin = true;
-      navToHome(email);
-    })
-    .catch((error) => toastHolder.errorCode(error.code, error));
+function notifyLogin() {
+  userStorage.isLogin = true;
+  loginListeners.forEach((listener) => listener(true));
 }
 
 function signInWithPopup() {
@@ -98,11 +45,6 @@ function signInWithPopup() {
     .catch((error) => {
       toastHolder.errorCode(error.code, error);
     });
-}
-
-function notifyLogin() {
-  userStorage.isLogin = true;
-  loginListeners.forEach((listener) => listener(true));
 }
 
 function logout() {
