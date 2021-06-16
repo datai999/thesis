a6fc701fe2505fa05ded93be2e76530d153b9d9a
Đức Anh Tài <datai28599@gmail.com>
2021-06-17 00:34:46 +0700

    version 6.17.00

--- src/env.js
+++ src/env.js
@@ -3,18 +3,16 @@ import { Platform } from "react-native";
 let isLogin = Platform.OS == "android" ? true : false;
 
 const local = {
-  domain: "http://localhost:19006/",
   baseURL: "http://192.168.15.112:8080/api",
   androidLinkDown:
     "https://drive.google.com/file/d/1Z6N8NdVO6vm0BNkRfPGr-GveHMTS9TLu",
-  version: "6.16.18",
+  version: "6.17.00",
   isLogin: isLogin,
   initialRouteName: isLogin ? "topic" : "login",
 };
 
 const staging = {
   ...local,
-  domain: "https://datai999.github.io/thesis/",
   baseURL: "https://datai-thesis.herokuapp.com/api",
 };
 
