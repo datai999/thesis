9c832ec5660f93edf9c09947b3a00b9a2c32918c
Đức Anh Tài <datai28599@gmail.com>
2021-06-16 13:31:01 +0700

    modal toast with animation

--- src/components/screen/TopNav.js
+++ src/components/screen/TopNav.js
@@ -1,11 +1,10 @@
 import {
   Button,
-  Divider,
   Layout,
+  Modal,
   Popover,
   Text,
   Toggle,
-  Tooltip,
   TopNavigation,
   TopNavigationAction
 } from "@ui-kitten/components";
@@ -20,6 +19,8 @@ import {
   MenuIcon
 } from "components/Icons";
 import React from "react";
+import { StyleSheet } from "react-native";
+import * as Animatable from "react-native-animatable";
 import { langHolder, toastHolder } from "utils";
 import i18n from "utils/i18n";
 import { navHolder } from "utils/nav";
@@ -123,6 +124,10 @@ const TopNav = () => {
 const ToolTopNav = () => {
   const [visible, setVisible] = React.useState(false);
   const [log, setLog] = React.useState({ type: "info", message: "" });
+  const animationRef = React.useRef();
+
+  const animationEnd = () =>
+    animationRef.current.zoomOut(500).then(() => setVisible(false));
 
   React.useEffect(() => {
     toastHolder.listeners.push(setLog);
@@ -132,48 +137,48 @@ const ToolTopNav = () => {
     if (log.message?.length > 0) {
       console.log(log);
       setVisible(true);
+      setTimeout(animationEnd, 1500);
     }
   }, [log]);
 
-  const renderAnchor = () => <Divider />;
-
-  const renderIcon = () => {
-    let icon = InfoIcon;
+  function renderIcon(props) {
     switch (log.type) {
+      case "info":
+        return <InfoIcon {...props} />;
       case "success":
-        icon = CheckMarkCircle2;
-        break;
+        return <CheckMarkCircle2 {...props} />;
       case "waring":
-        icon = AlertTriangleIcon;
-        break;
+        return <AlertTriangleIcon {...props} />;
       case "danger":
-        icon = CloseCircleIcon;
-        break;
+        return <CloseCircleIcon {...props} />;
     }
-    return (
-      <Button
-        status={log.type}
-        appearance="ghost"
-        accessoryLeft={icon}
-        style={{ marginVertical: -25, marginLeft: -30, marginRight: -20 }}
-      ></Button>
-    );
-  };
+  }
 
   return (
     <Layout>
       {TopNav()}
-      <Tooltip
-        anchor={renderAnchor}
-        visible={visible}
-        onBackdropPress={() => setVisible(false)}
-        accessoryLeft={renderIcon}
-        style={{ backgroundColor: "white" }}
-      >
-        <Text status={log.type}>{i18n.t(log.message)}</Text>
-      </Tooltip>
+      <Modal visible={visible} style={styles.toastModal}>
+        <Animatable.View animation="fadeInDownBig" ref={animationRef}>
+          <Button
+            status={log.type}
+            appearance="ghost"
+            accessoryLeft={renderIcon}
+            style={{ marginVertical: -25, marginLeft: -30, marginRight: -20 }}
+          >
+            {i18n.t(log.message)}
+          </Button>
+        </Animatable.View>
+      </Modal>
     </Layout>
   );
 };
+
+const styles = StyleSheet.create({
+  toastModal: {
+    flexDirection: "row",
+    position: "absolute",
+    top: "10%",
+  },
+});
 
 export default ToolTopNav;
