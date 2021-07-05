7291f009f8d76f956926a7db1034eb03c9284f76
Đức Anh Tài <datai28599@gmail.com>
2021-07-06 01:11:36 +0700

    version 7.06.01a

--- src/env.js
+++ src/env.js
@@ -2,7 +2,7 @@ const local = {
   baseURL: "http://192.168.15.106:8080/api",
   androidLinkDown:
     "https://drive.google.com/file/d/1Z6N8NdVO6vm0BNkRfPGr-GveHMTS9TLu",
-  version: "7.06.01",
+  version: "7.06.01a",
   isLogin: false,
   initialRouteName: "login",
 };
