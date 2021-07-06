3bdad21bc19185bad14da67dfa123ecf2f9ab5e2
Đức Anh Tài <datai28599@gmail.com>
2021-07-07 00:16:44 +0700

    update link down android app

--- src/env.js
+++ src/env.js
@@ -1,7 +1,7 @@
 const local = {
   baseURL: "http://192.168.15.106:8080/api",
   androidLinkDown:
-    "https://drive.google.com/file/d/1Z6N8NdVO6vm0BNkRfPGr-GveHMTS9TLu",
+    "https://exp-shell-app-assets.s3.us-west-1.amazonaws.com/android/%40datai28599/thesis-1b99d70512c44b3cb77ea5d649c4b2d9-signed.apk",
   version: "7.06.01",
   isLogin: false,
   initialRouteName: "login",
