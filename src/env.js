010c9467c5e64ac3dcd58115dbbf8706981ad62c
Đức Anh Tài <datai28599@gmail.com>
2021-07-07 21:08:09 +0700

    static local env

--- src/env.js
+++ src/env.js
@@ -1,5 +1,5 @@
 const local = {
-  baseURL: "http://192.168.15.106:8080/api",
+  baseURL: "http://localhost:8080/api",
   androidLinkDown:
     "https://exp-shell-app-assets.s3.us-west-1.amazonaws.com/android/%40datai28599/thesis-1b99d70512c44b3cb77ea5d649c4b2d9-signed.apk",
   version: "7.06.01",
