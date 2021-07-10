8a275cba60ac81df15ce9e2561c9d88fc0cd973f
Đức Anh Tài <datai28599@gmail.com>
2021-07-11 01:29:18 +0700

    screen group by topic create

--- src/pages/TopicScreen/topicCreateScreen.js
+++ src/pages/TopicScreen/topicCreateScreen.js
@@ -1,9 +1,11 @@
-import { Button, Layout } from "@ui-kitten/components";
+import { Button, Layout, Text } from "@ui-kitten/components";
+import StudentApi from "api/person/StudentApi";
+import TeacherApi from "api/person/TeacherApi";
 import TopicAssignApi from "api/topic/TopicAssignApi";
-import { MyInput } from "components/Input";
+import { MyAutocompleteTag, MyInput } from "components/Input";
 import { MyMultiSelect, MySelect } from "components/Select";
 import React from "react";
-import { ScrollView, StyleSheet } from "react-native";
+import { Dimensions, ScrollView, StyleSheet } from "react-native";
 import { IconButton } from "react-native-paper";
 import {
   createProps,
@@ -24,10 +26,17 @@ const submit = () => {
 };
 
 const FormLayout = () => {
+  const [dimensions, setDimensions] = React.useState(Dimensions.get("window"));
   const [language, setLanguage] = React.useState(i18n.languages);
   const [multiLang, setMultiLang] = React.useState(0);
 
   const propStore = createProps(form);
+
+  React.useEffect(() => {
+    Dimensions.addEventListener("change", (e) => {
+      setDimensions(e.window);
+    });
+  }, []);
 
   React.useEffect(() => {
     languageService.listen(setLanguage);
@@ -39,7 +48,6 @@ const FormLayout = () => {
         <Button
           size="tiny"
           status="primary"
-          style={{ margin: 5 }}
           onPress={() => setMultiLang(multiLang + 1)}
           accessoryRight={() => (
             <IconButton
@@ -56,32 +64,59 @@ const FormLayout = () => {
       </Layout>
 
       <ScrollView
-        style={{ maxHeigh: "50%" }}
-        contentContainerStyle={{ marginHorizontal: 24, marginVertical: 15 }}
+        style={styles.scroll}
+        contentContainerStyle={[
+          styles.scrollContent,
+          { flexDirection: dimensions.width > 1000 ? "row" : "column" },
+        ]}
       >
-        <MyInput {...propStore.inputLang("topic.name")} />
-        {multiLang > 0 && (
-          <MyInput {...propStore.inputToggleLang("topic.name")} />
-        )}
-        <MySelect {...propStore.select("topic.educationMethod")} />
-        <MyInput {...propStore.select("topic.semester")} />
-        <MyMultiSelect {...propStore.select("topic.major")} />
-        <MyInput {...propStore.input("topic.code")} />
-        <MySelect {...propStore.select("topic.minStudentTake")} />
-        <MySelect {...propStore.select("topic.maxStudentTake")} />
+        <Layout style={styles.group}>
+          <Text style={styles.headerGroup}>{i18n.t("origin.topicInfo")}</Text>
+          <MyInput {...propStore.inputLang("topic.name")} />
+          {multiLang > 0 && (
+            <MyInput {...propStore.inputToggleLang("topic.name")} />
+          )}
+          <MySelect {...propStore.select("topic.educationMethod")} />
+          <MyInput {...propStore.select("topic.semester")} />
+          <MyMultiSelect {...propStore.select("topic.major")} />
+          <MyInput {...propStore.input("topic.code")} />
+          <Layout
+            style={{ justifyContent: "space-evenly", flexDirection: "row" }}
+          >
+            <MySelect {...propStore.select("topic.minStudentTake")} />
+            <MySelect {...propStore.select("topic.maxStudentTake")} />
+          </Layout>
+        </Layout>
+        <Layout style={styles.group}>
+          <Text style={styles.headerGroup}>
+            {i18n.t("origin.topicDescription")}
+          </Text>
+          <MyInput {...propStore.inputLang("topic.description")} />
+          {multiLang > 0 && (
+            <MyInput {...propStore.inputToggleLang("topic.description")} />
+          )}
+          <MyInput {...propStore.inputLang("topic.topicTask")} />
+          {multiLang > 0 && (
+            <MyInput {...propStore.inputToggleLang("topic.topicTask")} />
+          )}
+          <MyInput {...propStore.inputLang("topic.thesisTask")} />
+          {multiLang > 0 && (
+            <MyInput {...propStore.inputToggleLang("topic.thesisTask")} />
+          )}
+        </Layout>
+        <Layout style={styles.group}>
+          <Text style={styles.headerGroup}>{i18n.t("origin.topicAssign")}</Text>
+          <MyAutocompleteTag
+            {...propStore.inputSearch("topicAssign.executeStudent", StudentApi)}
+          />
 
-        <MyInput {...propStore.inputLang("topic.description")} />
-        {multiLang > 0 && (
-          <MyInput {...propStore.inputToggleLang("topic.description")} />
-        )}
-        <MyInput {...propStore.inputLang("topic.topicTask")} />
-        {multiLang > 0 && (
-          <MyInput {...propStore.inputToggleLang("topic.topicTask")} />
-        )}
-        <MyInput {...propStore.inputLang("topic.thesisTask")} />
-        {multiLang > 0 && (
-          <MyInput {...propStore.inputToggleLang("topic.thesisTask")} />
-        )}
+          <MyAutocompleteTag
+            {...propStore.inputSearch("topicAssign.guideTeacher", TeacherApi)}
+          />
+          <MyAutocompleteTag
+            {...propStore.inputSearch("topicAssign.reviewTeacher", TeacherApi)}
+          />
+        </Layout>
       </ScrollView>
       <Button onPress={submit}>{i18n.t("origin.submit")}</Button>
     </Layout>
@@ -90,19 +125,32 @@ const FormLayout = () => {
 
 const styles = StyleSheet.create({
   container: {
-    margin: 10,
-    flex: 1,
+    marginVertical: "1%",
+    maxHeight: "95%",
+    alignItems: "center",
+    backgroundColor: "transparent",
   },
-  row: {
-    flexDirection: "row",
+  scroll: {
+    minWidth: "90%",
+    maxHeight: "100%",
+    marginVertical: "1%",
+    backgroundColor: "transparent",
   },
-  left: { flex: 1 },
-  right: { flex: 1 },
-  tagList: {
-    flexDirection: "row",
+  scrollContent: {
+    marginVertical: 15,
+    justifyContent: "space-evenly",
   },
-  tag: {
-    marginHorizontal: 2,
+  group: {
+    minWidth: "30%",
+    maxWidth: "90%",
+    padding: "1%",
+    // backgroundColor: "transparent",
+  },
+  headerGroup: {
+    margin: 5,
+    fontWeight: "bold",
+    textAlign: "center",
+    fontSize: 15,
   },
 });
 
