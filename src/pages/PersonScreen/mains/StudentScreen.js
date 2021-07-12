75ff152990297f823a524ade4517557efde1cfe9
Đức Anh Tài <datai28599@gmail.com>
2021-07-12 14:40:26 +0700

    apply setting column visibe to person table

--- src/pages/PersonScreen/mains/StudentScreen.js
+++ src/pages/PersonScreen/mains/StudentScreen.js
@@ -2,21 +2,48 @@ import StudentApi from "api/person/StudentApi";
 import { StudentForm } from "components/form";
 import OverTable from "components/screen/OverTable";
 import React from "react";
+import { Dimensions } from "react-native";
 
-const arrLink = [
-  "person.code",
-  "person.name",
-  "person.gender",
-  "person.educationMethod",
-  "person.major",
-  "person.email",
-  "person.phone",
+const width = Dimensions.get("window").width;
+
+const defaultFields = [
+  {
+    link: "person.code",
+    visible: true,
+  },
+  {
+    link: "person.name",
+    visible: true,
+  },
+  {
+    link: "person.gender",
+    visible: width > 500,
+  },
+  {
+    link: "person.educationMethod",
+    visible: width > 500,
+  },
+  {
+    link: "person.major",
+    visible: width > 500,
+  },
+  {
+    link: "person.email",
+    visible: width > 700,
+  },
+  {
+    link: "person.phone",
+    visible: width > 800,
+  },
 ];
 
 const overTableProps = {
-  links: arrLink,
+  tableName: "student",
   form: StudentForm,
   api: StudentApi,
+  defaultProps: {
+    fields: defaultFields,
+  },
 };
 
 const StudentScreen = () => {
