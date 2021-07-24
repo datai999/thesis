a9a7c917e96174375da80022c21e5e269fb7ce95
Đức Anh Tài <datai28599@gmail.com>
2021-07-24 14:55:35 +0700

    add input lang to props service

--- src/service/propsService.js
+++ src/service/propsService.js
@@ -1,5 +1,6 @@
 import constData from "data/constData";
 import _ from "lodash";
+import { i18n } from "utils";
 
 export default {
   createPropsStore: (defaultForm) => {
@@ -7,15 +8,29 @@ export default {
 
     const inputProps = (path, props) => {
       return {
+        label: path + ".label",
+        placeholder: path + ".placeholder",
         value: _.get(form, path),
         callBack: (value) => _.set(form, path, value),
         ...props,
       };
     };
 
+    const inputLang = (path) => {
+      const langText =
+        i18n.language == "en" ? i18n.t("origin.inEn") : i18n.t("origin.inVi");
+      const cloneInputProps = inputProps(path);
+      return {
+        ...cloneInputProps,
+        label: i18n.t(cloneInputProps.label) + langText,
+        placeholder: i18n.t(cloneInputProps.placeholder) + langText,
+      };
+    };
+
     return {
       form,
       input: inputProps,
+      inputLang,
       inputSearch: (path, api) => {
         return {
           ...inputProps(path),
