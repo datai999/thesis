363cd5d92eeacda1afca634dfd9efccc145b21d5
Đức Anh Tài <datai28599@gmail.com>
2021-07-26 23:01:37 +0700

    init introduction

--- src/pages/presentScreen/components/introduction.js
+++ src/pages/presentScreen/components/introduction.js
@@ -1,7 +1,31 @@
-import { StyleSheet } from "react-native";
+import { Layout, Text } from "@ui-kitten/components";
+import React from "react";
 
 export default {
   title: "Giới thiệu",
+  body: () => {
+    return (
+      <Layout>
+        <Text>
+          {"\t"}
+          Năm 2020-2021 là những năm đầy biến động, khó khăn và những thách thức
+          lớn, đại dịch COVID-19 đã làm thay đổi xã hội, thay đổi nền kinh tế,
+          giáo dục của nhiều nước trên thế giới.
+        </Text>
+        <Text>
+          {"\n\t"}
+          Việc số hóa tài liệu trở nên cần thiết hơn. Cần công nghệ hóa các
+          phương pháp làm việc cũng như các dịch vụ hoạt động ổn định với lượng
+          người dùng chuyển đột ngột từ môi trường thực tại qua internet.
+        </Text>
+        <Text>
+          {"\n\t"}
+          Nhận thấy rằng hệ thống đánh giá luận văn của trường Đại học Bách khoa
+          - Đại học Quốc gia TP.HCM cần được phát triển để có thể đáp ứng với
+          nhiều khó khăn trong tương lai hơn. Đó là lý do chính nhóm đề xuất đề
+          tài "Xây dưng hê thông đanh gia luân văn tôt nghiêp"
+        </Text>
+      </Layout>
+    );
+  },
 };
-
-const styles = StyleSheet.create({});
