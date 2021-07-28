b63134ce5b5c9cab87e8c9859601f42677cf8f16
Đức Anh Tài <datai28599@gmail.com>
2021-07-28 09:40:03 +0700

    version 7.28.09

--- src/env.js
+++ src/env.js
@@ -2,7 +2,7 @@ const local = {
   baseURL: "http://192.168.3.101:8080/api",
   androidLinkDown:
     "https://exp-shell-app-assets.s3.us-west-1.amazonaws.com/android/%40datai28599/thesis-ef476fae41fc4b70a5b735ca0109afec-signed.apk",
-  version: "7.27.23",
+  version: "7.28.09",
   // skipLogin: {
   //   email: "tai.nguyen.cse.datai",
   //   code: "123",
